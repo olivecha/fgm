45fe30ace8d719ccdaeb95eb7a175e1dc3f80a2f
Olivier Chabot <olivierchabot918@gmail.com>
2025-06-13 09:07:33 -0400

    update reader

--- fgmanifold/fgmreader.py
+++ fgmanifold/fgmreader.py
@@ -3,6 +3,9 @@ import numpy as np
 
 class FGManifoldManager(object):
 
+    Y_O2_IN = 0.22844261066374683
+    Y_O2_OUT = 0.13601618257237072
+
     def __init__(self, filepath):
         """
         Read FGM data from {filepath} and initialize the FGM structure
@@ -18,7 +21,7 @@ class FGManifoldManager(object):
         """
         Finds the 1D flame closest to the phi value
         """
-        return self.flames[np.argmin(np.abs(self.phi_values - phi))]
+        return self.flames[np.argmin(np.abs(np.array(self.phi_values) - phi))]
 
     def Sl_at_phi(self, phi):
         """
@@ -31,4 +34,10 @@ class FGManifoldManager(object):
         Return the thermal flame thickness at phi
         """
         return self.flame_at_phi(phi).Lt
+
+
+    def C(self, flame):
+        Y_O2 = flame.Y[flame.gas.species_index('O2')]
+        return (Y_O2 - self.Y_O2_IN) / (self.Y_O2_OUT - self.Y_O2_IN)
+        
     
